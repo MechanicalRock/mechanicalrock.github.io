29345aca10d8ae737e89af11226893206a6efa12
Thashin <Thashin@live.com>
2021-04-30 18:53:06 +0800

    Jest tests for paginate

--- js/paginate.js
+++ js/paginate.js
@@ -1,19 +1,17 @@
-const VISIBLE_PAGE_NUMBERS = 10;
-
-function setupPagination() 
+function setupPagination(visiblePageNumbers) 
 {
     var currentPageNumber = getCurrentPageNumber();
-    var offsetPages = Math.floor((currentPageNumber - 1)/VISIBLE_PAGE_NUMBERS) * VISIBLE_PAGE_NUMBERS;
+    var offsetPages = Math.floor((currentPageNumber - 1)/visiblePageNumbers) * visiblePageNumbers;
     var startingPageNumber = offsetPages + 1
     var listElements = [];
-    if(currentPageNumber > VISIBLE_PAGE_NUMBERS)
+    if(currentPageNumber > visiblePageNumbers)
     {
-        var previousSetOfPagesStartPage = startingPageNumber - VISIBLE_PAGE_NUMBERS
+        var previousSetOfPagesStartPage = startingPageNumber - visiblePageNumbers
         var previousSetOfPagesElement = getSetOfPagesElement(previousSetOfPagesStartPage, "<<"); 
         listElements = [...listElements, previousSetOfPagesElement]
     }
 
-    for (pageNumber = startingPageNumber; pageNumber <= totalPages && pageNumber < startingPageNumber + VISIBLE_PAGE_NUMBERS ; pageNumber ++)
+    for (pageNumber = startingPageNumber; pageNumber <= totalPages && pageNumber < startingPageNumber + visiblePageNumbers ; pageNumber ++)
     {
         listElement = setupPageButton(pageNumber, pageNumber == currentPageNumber)
         listElements = [...listElements, listElement]
@@ -21,7 +19,7 @@ function setupPagination()
 
     if(currentPageNumber < totalPages)
     {
-        var nextSetOfPagesStartPage = startingPageNumber + VISIBLE_PAGE_NUMBERS;
+        var nextSetOfPagesStartPage = startingPageNumber + visiblePageNumbers;
         var nextSetOfPagesElement = getSetOfPagesElement(nextSetOfPagesStartPage, ">>"); 
         listElements = [...listElements, nextSetOfPagesElement]
     }
@@ -36,8 +34,8 @@ function getCurrentPageNumber()
 {
     var pathName = window.location.pathname;
     pathName = pathName.substring(0, pathName.length - 1)
-    var currentPageNumber = pathName == "" ? 1 : pathName.split("/").pop();
-    currentPageNumber = currentPageNumber > totalPages ? 1 : currentPageNumber;
+    var currentPageNumber = pathName == "" ? "1" : pathName.split("/").pop();
+    currentPageNumber = currentPageNumber > totalPages ? "1" : currentPageNumber;
 
     return currentPageNumber;
 }
@@ -46,14 +44,7 @@ function getSetOfPagesElement(startOfPageNumbers, label)
 {
     var setOfPagesElement = document.createElement("a");
     setOfPagesElement.innerText = label;
-    if(startOfPageNumbers == 1)
-    {
-        setOfPagesElement.setAttribute("href", "/");
-    }
-    else
-    {
-        setOfPagesElement.setAttribute("href", paginatePath + startOfPageNumbers);
-    }
+    setHrefForPage(setOfPagesElement, startOfPageNumbers)
     setOfPagesElement.setAttribute("onclick", "setupPagination()");
     setOfPagesElement.className = "change-pages-set";
     var listElement = document.createElement("li");
@@ -66,18 +57,31 @@ function setupPageButton(pageNumber, isActive)
 {
     var pageNumberElement = document.createElement("a");
     pageNumberElement.innerText = pageNumber;
-    if(pageNumber == 1)
-    {
-        pageNumberElement.setAttribute("href", "/");
-    }
-    else
-    {
-        pageNumberElement.setAttribute("href", paginatePath + pageNumber );
-    }
+    setHrefForPage(pageNumberElement, pageNumber)
 
     pageNumberElement.className = isActive ? "page active" : "page";
     var listElement = document.createElement("li");
     listElement.appendChild(pageNumberElement);
 
     return listElement;
+}
+
+function setHrefForPage(element, pageNumber)
+{
+    if(pageNumber == 1)
+    {
+        element.setAttribute("href", "/");
+    }
+    else
+    {
+        element.setAttribute("href", paginatePath + pageNumber );
+    }
+}
+
+module.exports = {
+    setHrefForPage,
+    setupPageButton,
+    getSetOfPagesElement,
+    getCurrentPageNumber,
+    setupPagination
 }
